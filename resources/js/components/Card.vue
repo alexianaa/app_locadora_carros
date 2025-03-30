4e5a877a6e9bd9d1679132b24b6e3e44b1fa4c2b
Alexia Cardoso <alexiasilvacardosoo@gmail.com>
2025-03-30 12:18:58 -0300

    refatora componentes

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card mb-3">
+    <div class="card-header">{{ titulo }}</div>
+
+    <div class="card-body">
+      <slot name="conteudo"></slot>
+    </div>
+
+    <div class="card-footer d-grid d-md-flex justify-content-md-end"> 
+      <slot name="rodape"></slot>
+    </div>
+      
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['titulo']
+  }
+</script>
--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -2,40 +2,47 @@
   <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Buscar de marcas</div>
 
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col mb-3">
-                          <input-component 
-                            titulo="ID" 
-                            id="inputId"
-                            help="idHelp"
-                            texto-ajuda="Opcional. Informe o ID da Marca"
-                          > 
-                            <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                          </input-component>
-                        </div>
-                        <div class="col mb-3">
-                          <input-component 
-                            titulo="Nome" 
-                            id="inputNome"
-                            help="nomeHelp"
-                            texto-ajuda="Opcional. Informe o Nome da marca"
-                          > 
-                            <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                          </input-component>
-                        </div>
+          <!-- BUSCA DE MARCAS -->
+           <card-component titulo="Buscar marcas">
+              <template v-slot:conteudo>
+                <div class="row">
+                    <div class="col mb-3">
+                      <input-component 
+                        titulo="ID" 
+                        id="inputId"
+                        help="idHelp"
+                        texto-ajuda="Opcional. Informe o ID da Marca"
+                      > 
+                        <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                      </input-component>
                     </div>
-
+                    <div class="col mb-3">
+                      <input-component 
+                        titulo="Nome" 
+                        id="inputNome"
+                        help="nomeHelp"
+                        texto-ajuda="Opcional. Informe o Nome da marca"
+                      > 
+                        <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                      </input-component>
+                    </div>
                 </div>
+              </template>
+              <template v-slot:rodape>
+                <button type="submit" class="btn btn-primary btn-sm">Pesquisar</button>
+              </template>
+           </card-component>
 
-                <div class="card-footer d-grid d-md-flex justify-content-md-end"> 
-                    <button type="submit" class="btn btn-primary btn-sm">Pesquisar</button>
-                </div>
-
-            </div>
+            <!-- LISTAGEM DE MARCAS -->
+            <card-component titulo="Relação de marcas">
+              <template v-slot:conteudo>
+                <table-component></table-component>
+              </template>
+              <template v-slot:rodape>
+                <button type="submit" class="btn btn-primary btn-sm">Adicionar</button>
+              </template>
+            </card-component>
         </div>
     </div>
   </div>
