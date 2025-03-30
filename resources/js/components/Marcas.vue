748b636d2a658c453f6d79b71bdedda62b41fb79
Alexia Cardoso <alexiasilvacardosoo@gmail.com>
2025-03-30 11:02:21 -0300

    corrige rota para marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -3,10 +3,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Marcas</div>
+                <div class="card-header">Buscar de marcas</div>
 
                 <div class="card-body">
-                  registros
+                  <div class="mb-3">
+                    <label for="inputID" class="form-label">ID</label>
+                    <input type="number" class="form-control" id="inputID" aria-describedby="idHelp" placeholder="ID">
+                    <div id="idHelp" class="form-text">Opcional. Informe o ID do registro</div>
+                  </div>
+                  <div class="mb-3">
+                    <label for="inputNome" class="form-label">Nome</label>
+                    <input type="number" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                    <div id="nomeHelp" class="form-text">Opcional. Informe o nome da marca</div>
+                  </div>
+                  <button type="submit" class="btn btn-primary btn-sm">Pesquisar</button>
                 </div>
             </div>
         </div>
